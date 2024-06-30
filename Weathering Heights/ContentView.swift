9d89bcc7386f3113a6438a76bfe4017eced88495
H708-dot <78126028+H708-dot@users.noreply.github.com>
2024-06-30 12:37:53 +0400

    Update ContentView.swift

--- Weathering Heights/ContentView.swift
+++ Weathering Heights/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("Background").frame(minWidth: 0, maxWidth: .infinity).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Image("logo")
+                .padding(.top, -300.0)
+            VStack {
+                Text("""
+                     Welcome to
+                     Weather-ing Heights
+                     """).font(.largeTitle).bold().foregroundStyle(.white)
+                Divider()
+                Text("""
+    Dive into weather updates while exploring quizzes and activities designed to promote climate action.
+    Let's work together to make a positive impact on our planet, one step at a time.
+    """).font(.title2).fontWeight(.light).padding(.bottom, -290.0).frame(width: 315.0).foregroundStyle(.white)
+            }
         }
-        .padding()
+        
     }
 }
 
