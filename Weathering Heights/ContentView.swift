a36135e6148babfefb95e4178d14750b48b21f07
H708-dot <78126028+H708-dot@users.noreply.github.com>
2024-06-30 17:14:00 +0400

    Button Added

--- Weathering Heights/ContentView.swift
+++ Weathering Heights/ContentView.swift
@@ -9,26 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Image("Background").frame(minWidth: 0, maxWidth: .infinity).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Image("logo")
-                .padding(.top, -300.0)
-            VStack {
-                Text("""
-                     Welcome to
-                     Weather-ing Heights
-                     """).font(.largeTitle).bold().foregroundStyle(.white)
-                Divider()
-                Text("""
-    Dive into weather updates while exploring quizzes and activities designed to promote climate action.
-    Let's work together to make a positive impact on our planet, one step at a time.
-    """).font(.title2).fontWeight(.light).padding(.bottom, -290.0).frame(width: 315.0).foregroundStyle(.white)
+        NavigationView{
+            
+            ZStack {
+                Image("Background").frame(minWidth: 0, maxWidth: .infinity).edgesIgnoringSafeArea(.all)
+                
+                Image("logo")
+                    .padding(.top, -300.0)
+                
+                VStack {
+                    Text("""
+                         Welcome to
+                         Weather-ing Heights
+                         """)
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundStyle(.white)
+                    .padding(.top, 200.0)
+                    
+                    Divider()
+                    
+                    Text("""
+        Dive into weather updates while exploring quizzes and activities designed to promote climate action.
+        Let's work together to make a positive impact on our planet, one step at a time.
+        """)
+                    .font(.title2)
+                    .fontWeight(.light)
+                    .padding(.bottom)
+                    .frame(width: /*@START_MENU_TOKEN@*/315.0/*@END_MENU_TOKEN@*/)
+                    .foregroundStyle(.white)
+                    
+                    Divider()
+                    Divider()
+                    
+                    NavigationLink(destination: SignUp()){Text("Next →")}.frame(width: 200, height: 50, alignment: .center).background(Color(UIColor(red: 100/255, green: 100/255, blue: 100/255, alpha: 1))).foregroundColor(.white).cornerRadius(8)
+                    
+                    
+                    
+                }
             }
         }
-        
     }
 }
-
 #Preview {
     ContentView()
 }
